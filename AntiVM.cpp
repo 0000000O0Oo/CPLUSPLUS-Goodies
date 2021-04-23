7af4bd683ab10fd80e161f2d14f375601b7b3197
k0rup710n <61102077+k0rup710n@users.noreply.github.com>
2021-04-23 19:42:02 -0400

    Update AntiVM.cpp

--- AntiVM.cpp
+++ AntiVM.cpp
@@ -1,54 +1,54 @@
-void antiVirtualBox(){
-    if(fileExists("C:\\Windows\\System32\\Drivers\\VBoxMouse.sys")){
-        exit(EXIT_FAILURE);
-    }
-    else if(fileExists("C:\\windows\\System32\\Drivers\\VBoxGuest.sys")) exit(EXIT_FAILURE);
-	else if(fileExists("C:\\windows\\System32\\Drivers\\VBoxSF.sys")) exit(EXIT_FAILURE);
-	else if(fileExists("C:\\windows\\System32\\Drivers\\VBoxVideo.sys")) exit(EXIT_FAILURE);
-	else if(fileExists("C:\\windows\\System32\\vboxdisp.dll")) exit(EXIT_FAILURE);
-	else if(fileExists("C:\\windows\\System32\\vboxhook.dll")) exit(EXIT_FAILURE);
-	else if(fileExists("C:\\windows\\System32\\vboxmrxnp.dll")) exit(EXIT_FAILURE);
-	else if(fileExists("C:\\windows\\System32\\vboxogl.dll")) exit(EXIT_FAILURE);
-	else if(fileExists("C:\\windows\\System32\\vboxoglarrayspu.dll")) exit(EXIT_FAILURE);
-	else if(fileExists("C:\\windows\\System32\\vboxoglcrutil.dll")) exit(EXIT_FAILURE);
-	else if(fileExists("C:\\windows\\System32\\vboxoglerrorspu.dll")) exit(EXIT_FAILURE);
-	else if(fileExists("C:\\windows\\System32\\vboxoglfeedbackspu.dll")) exit(EXIT_FAILURE);
-	else if(fileExists("C:\\windows\\System32\\vboxoglpackspu.dll")) exit(EXIT_FAILURE);
-	else if(fileExists("C:\\windows\\System32\\vboxoglpassthroughspu.dll")) exit(EXIT_FAILURE);
-}
-
-void antiVmware(){
-	if(fileExists("C:\\windows\\System32\\Drivers\\Vmmouse.sys")){
-		exit(EXIT_FAILURE);
+void virtualBoxEscape() {
+		DWORD fileAttribute = GetFileAttributesA("C:\\Windows\\System32\\Drivers\\VBoxMouse.sys");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\Drivers\\VBoxGuest.sys");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\Drivers\\VBoxSF.sys");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\Drivers\\VBoxVideo.sys");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\vboxdisp.dll");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\vboxhook.dll");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\vboxmrxnp.dll");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\vboxogl.dll");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\vboxoglarrayspu.dll");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\vboxoglcrutil.dll");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\vboxoglerrorspu.dll");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\vboxoglfeedbackspu.dll");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\vboxoglpackspu.dll");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\vboxoglpassthroughspu.dll");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
 	}
-	else if(fileExists("C:\\windows\\System32\\Drivers\\vm3dgl.dll")){
-		exit(EXIT_FAILURE);
+	void VMWareEscape() {
+		DWORD fileAttribute = GetFileAttributesA("C:\\windows\\System32\\Drivers\\Vmmouse.sys");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\Drivers\\vm3dgl.dll");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\Drivers\\vmdum.dll");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\Drivers\\vm3dver.dll");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\Drivers\\vmtray.dll");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\Drivers\\VMToolsHook.dll");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\Drivers\\vmmousever.dll");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\Drivers\\vmhgfs.dll");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\Drivers\\vmGuestLib.dll");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\Drivers\\VmGuestLibJava.dll");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
+		fileAttribute = GetFileAttributesA("C:\\windows\\System32\\Driversvmhgfs.dll");
+		if (fileAttribute != INVALID_FILE_ATTRIBUTES && !(fileAttribute & FILE_ATTRIBUTE_DIRECTORY)) exit(EXIT_FAILURE);
 	}
-	else if(fileExists("C:\\windows\\System32\\Drivers\\vmdum.dll")){
-		exit(EXIT_FAILURE);
-	}
-	else if(fileExists("C:\\windows\\System32\\Drivers\\vm3dver.dll")){
-		exit(EXIT_FAILURE);
-	}
-	else if(fileExists("C:\\windows\\System32\\Drivers\\vmtray.dll")){
-		exit(EXIT_FAILURE);
-	}
-	else if(fileExists("C:\\windows\\System32\\Drivers\\VMToolsHook.dll")){
-		exit(EXIT_FAILURE);
-	}
-	else if(fileExists("C:\\windows\\System32\\Drivers\\vmmousever.dll")){
-		exit(EXIT_FAILURE);
-	}
-	else if(fileExists("C:\\windows\\System32\\Drivers\\vmhgfs.dll")){
-		exit(EXIT_FAILURE);
-	}
-	else if(fileExists("C:\\windows\\System32\\Drivers\\vmGuestLib.dll")){
-		exit(EXIT_FAILURE);
-	}
-	else if(fileExists("C:\\windows\\System32\\Drivers\\VmGuestLibJava.dll")){
-		exit(EXIT_FAILURE);
-	}
-	else if(fileExists("C:\\windows\\System32\\Driversvmhgfs.dll")){
-		exit(EXIT_FAILURE);
-	}
-}
